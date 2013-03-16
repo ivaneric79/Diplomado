69ea05e821ae7f8eb85849422bf94e0f7cf4b9d0
ivaneric79 <ivaneric79@gmail.com>
2013-03-16 12:17:10 -0600

    2

--- js/API/almacenamiento.js
+++ js/API/almacenamiento.js
@@ -45,14 +45,54 @@ function getId(tab){
 	return regreso+1;
 }
 
-function guardarReservaciones(id,habs,pers,dias,tipo){
+function leerHistorial(tab){
+	accesoBD().transaction(function(tx){
+		tx.executeSql('SELECT * FROM '+tab,[], function(tx1,resultado){
+			largo = resultado.rows.length;
+			if(largo!=0){
+			for (i=0;i<largo;i++){
+				$('#historial div[data-role=content]').append('	<div data-role="collapsible-set"><div data-role="collapsible" data-collapsed="true">'
+				+ '<h3>'+resultado.rows.item(i).fecha+'</h3><strong>Habitaciones:</strong> '+resultado.rows.item(i).habitaciones+'</br><strong>Personas:</strong> '+resultado.rows.item(i).personas+'<br /><strong>Estancia:</strong>'+resultado.rows.item(i).dias+'<br />'+'</div></div>');
+			
+				
+				}
+				}else{
+					$('#historial div[data-role=content]').text('<h2><No hay reservas anteriores</h2>');
+					}
+			//var registro1=resultado.rows.item(0).rId;
+		}, function(err){
+			alert(err.code);
+		});
+	}, function(err){
+		alert(err.code);	
+	}, function(){
+		//alert('ok');
+	});
+	return regreso+1;
+}
+
+function guardarReservaciones(habs,pers,dias,tipo){
 	//Insertar registro en la tabla de reservas
 	var f=new Date();
 	var fecha = f.getDate()+'/'+f.getMonth()+'/'+f.getFullYear();
 	accesoBD().transaction(function(tx){
-		tx.executeSql('INSERT INTO reserva (rId, fecha, habitaciones, personas, dias, tipo) VALUES ('+id+',"'+fecha+'","'+habs+'","'+pers+'","'+dias+'","'+tipo+'")');
+		tx.executeSql('INSERT INTO reserva ( fecha, habitaciones, personas, dias, tipo) VALUES ('+fecha+'","'+habs+'","'+pers+'","'+dias+'","'+tipo+'")');
+	guardarHistorial(habs,pers,dias);
 	},function(err){
 		pgAlert('Error al guardar la Reserva',err.code);
+	},function(){
+		pgAlert('Reserva Guardada','Esperando por conexión a Internet');
+	});
+}
+
+function guardarHistorial(habs,pers,dias){
+	//Insertar registro en la tabla de historial
+	var f=new Date();
+	var fecha = f.getDate()+'/'+f.getMonth()+'/'+f.getFullYear();
+	accesoBD().transaction(function(tx){
+		tx.executeSql('INSERT INTO historial ( fecha, habitaciones, personas, dias) VALUES ('+fecha+'","'+habs+'","'+pers+'","'+dias+'")');
+	},function(err){
+		pgAlert('Error al guardar la historial',err.code);
 	},function(){
 		pgAlert('Reserva Guardada','Esperando por conexión a Internet');
 	});
